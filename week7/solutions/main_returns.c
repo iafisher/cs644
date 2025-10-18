ec5c8f9313e0a40d3b32fc2255c8b230c938d2f3
Ian Fisher <ian@iafisher.com>
2025-10-18 11:47:09 +0000

    week7: solutions

---
+++ week7/solutions/main_returns.c
@@ -0,0 +1,23 @@
+/**
+ * https://iafisher.com/cs644/fall2025/week7
+ */
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "cs644.h"
+
+void* mythread(void* p) {
+  puts("thread 1 running");
+  sleep(3);
+  puts("thread 1 exiting");
+  return NULL;
+}
+
+int main() {
+  pthread_t thrd;
+  int r = pthread_create(&thrd, NULL, mythread, NULL);
+  if (r != 0) { cs644_bail("pthread_create failed"); }
+  sleep(1);
+  puts("main exiting");
+  return 0;
+}

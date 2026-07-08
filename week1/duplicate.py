8e367349d8ce32c134d40bc65a6f71f488f84ecd
Ian Fisher <ian@iafisher.com>
2026-07-07 22:28:36 -0400

    week1: solutions

---
+++ week1/duplicate.py
@@ -0,0 +1,65 @@
+"""
+Solution to the in-class exercise "Duplicating a file" for week 1 of CS644.
+
+https://iafisher.com/cs644/summer2026/week1-files-pt-1
+"""
+
+import argparse
+import os
+
+
+def duplicate(inpath: str, outpath: str, *, block_size: int = 4096) -> None:
+    ifd = os.open(inpath, os.O_RDONLY)
+    # `os.O_EXCL` causes `os.open` to raise an error if the file already exists,
+    # ensuring we do not overwrite an existing file.
+    #
+    # The third argument to `os.open` sets the permissions of the newly-created
+    # file. Unix file permissions are covered in Week 2.
+    ofd = os.open(outpath, os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0o644)
+
+    try:
+        # `os.read` reads a fixed number of bytes, but the size of the contents of
+        # `inpath` is variable. We could call `os.read` with a very large number,
+        # but (a) this could still fail on a very large file, and (b) it would be
+        # wasteful of memory.
+        #
+        # Solution: Call `os.read` and `os.write` in a loop until the end of the
+        # file is reached.
+        while True:
+            b = os.read(ifd, block_size)
+            if len(b) == 0:
+                break
+            os.write(ofd, b)
+    finally:
+        # For this particular program, it's not really important to call `os.close`
+        # as the operating system will clean up any open file descriptors when the
+        # program exits. However, it's a good habit to get into.
+        os.close(ifd)
+        os.close(ofd)
+
+
+def duplicate_range(inpath: str, outpath: str, *, range_start: int, range_end: int) -> None:
+    ifd = os.open(inpath, os.O_RDONLY)
+    ofd = os.open(outpath, os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0o644)
+
+    try:
+        os.lseek(ifd, range_start, os.SEEK_SET)
+        # For simplicity, I make one call to `os.read` with the whole range since
+        # we know ahead of time how many bytes we want to copy.
+        #
+        # In a real-world program, it would be advisable to read in smaller chunks
+        # in case `range_end - range_start + 1` is very large.
+        b = os.read(ifd, range_end - range_start + 1)
+        os.write(ofd, b)
+    finally:
+        os.close(ifd)
+        os.close(ofd)
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("inpath")
+    ap.add_argument("outpath")
+    args = ap.parse_args()
+
+    duplicate(args.inpath, args.outpath)
---
+++ week1/line_by_line.py
@@ -0,0 +1,80 @@
+"""
+Solution to the bonus exercise for week 1 of CS644:
+
+    Using `os.read`, write a Python function or class that reads a file
+    line-by-line.
+
+https://iafisher.com/cs644/summer2026/week1-files-pt-1
+"""
+
+import argparse
+import os
+
+
+DEFAULT_CHUNK_SIZE = 4096
+
+
+class LineReader:
+    def __init__(self, fd: int, *, chunk_size = DEFAULT_CHUNK_SIZE) -> None:
+        self.fd = fd
+        self.buffer = bytearray()
+        self.eof = False
+        self.chunk_size = chunk_size
+
+    def read_line(self) -> bytes:
+        if self.eof:
+            return b""
+
+        while True:
+            # We want to read the file line-by-line, but `os.read` will only read a
+            # fixed number of bytes.
+            #
+            # Solution: Store what we read in a buffer. If it undershot (did not read
+            # until the end of the line), then keep reading into the buffer. If it
+            # overshot (read past the end of the line), then take the line and leave
+            # the remainder in the buffer.
+
+            # First, see if the next line is entirely in the buffer.
+            newline_index = self.buffer.find(b"\n")
+            if newline_index != -1:
+                # If so, simply return it.
+                return self._consume_from_buffer(newline_index + 1)
+            else:
+                # Edge case: We've reached the end of the file and it does not end
+                # with a newline. We'll consider the trailing bytes to be the final
+                # line and return that.
+                if self.eof:
+                    return self._consume_from_buffer(len(self.buffer))
+
+                # Otherwise, read more into the buffer and loop.
+                self._read_into_buffer(self.chunk_size)
+
+    def _read_into_buffer(self, n: int) -> None:
+        b = os.read(self.fd, n)
+        if len(b) == 0:
+            self.eof = True
+        else:
+            self.buffer += b
+
+    def _consume_from_buffer(self, n: int) -> bytes:
+        r = self.buffer[:n]
+        self.buffer = self.buffer[n:]
+        return bytes(r)
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("filepath")
+    ap.add_argument("--chunk-size", type=int, default=DEFAULT_CHUNK_SIZE)
+    args = ap.parse_args()
+
+    fd = os.open(args.filepath, os.O_RDONLY)
+    try:
+        reader = LineReader(fd, chunk_size=args.chunk_size)
+        while True:
+            line = reader.read_line()
+            if len(line) == 0:
+                break
+            print(repr(line))
+    finally:
+        os.close(fd)
